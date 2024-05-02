8edc14d8ef68ef4160a6c19b75554263f2c85e24
Anthony Wong <wong.anthonyjordan@gmail.com>
2024-05-02 02:52:29 -0700

    Completed profile page, partially completed sidebar

--- bitlink-app/src/app/app.routes.ts
+++ bitlink-app/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {LoginPageComponent} from "./pages/login-page/login-page.component";
 import {RegistrationPageComponent} from "./pages/registration-page/registration-page.component";
+import {ProfilePageComponent} from "./pages/profile-page/profile-page.component";
 
 export const routes: Routes = [
   { path: 'login', component: LoginPageComponent },
-  { path: 'register', component: RegistrationPageComponent}
+  { path: 'register', component: RegistrationPageComponent },
+  { path: 'profile', component: ProfilePageComponent }
 ];
