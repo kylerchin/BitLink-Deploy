19c4da40f7f9e91926fa82e39dfc322f46046b9a
SusLiu03 <susannl5@uci.edu>
2024-05-02 05:05:06 -0700

    finished sidebar, working on notifs

---
+++ bitlink-app/src/app/pages/notification-page/notification-page.component.ts
@@ -0,0 +1,20 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { NewNotificationComponent } from '../../components/new-notification/new-notification.component';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+
+@Component({
+  selector: 'app-notification-page',
+  standalone: true,
+  imports: [NgOptimizedImage, NewNotificationComponent, SidebarComponent],
+  templateUrl: './notification-page.component.html',
+  styleUrl: './notification-page.component.scss',
+  encapsulation: ViewEncapsulation.None,
+})
+export class NotificationPageComponent {
+  username: string = 'Ferris the Crab';
+  usertag: string = 'RustyRustacean';
+  profile_pic: string = 'assets/ferris-on-pattern.jpg';
+  action: string = 'has liked your post "My experience with R coders...';
+  action2: string = 'has started following you';
+}
