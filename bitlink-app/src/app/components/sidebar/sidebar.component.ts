d09241f7c96f7dfb1b9861c895cd0fc2e02f9ff7
SusLiu03 <susannl5@uci.edu>
2024-05-03 02:49:16 -0700

    made skeleton of create a post popup

--- bitlink-app/src/app/components/sidebar/sidebar.component.ts
+++ bitlink-app/src/app/components/sidebar/sidebar.component.ts
@@ -1,17 +1,23 @@
 import { Component } from '@angular/core';
-import {NgOptimizedImage} from "@angular/common";
-
+import { NgOptimizedImage, CommonModule } from '@angular/common';
+import { CreatePostPopupComponent } from '../create-post-popup/create-post-popup.component';
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [
-    NgOptimizedImage
-  ],
+  imports: [NgOptimizedImage, CommonModule, CreatePostPopupComponent],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
-profileImagePath: string = "assets/profile_picture.png";
-name: string = "Jonny Boy";
-username: string = "@John BitLink";
+  profileImagePath: string = 'assets/profile_picture.png';
+  name: string = 'Jonny Boy';
+  username: string = '@John BitLink';
+  show = false;
+  openpopup() {
+    this.show = true;
+    console.log('clicked');
+  }
+  popupclosed(show: boolean) {
+    this.show = show;
+  }
 }
