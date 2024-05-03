a7d5564d6129c205eccfc9e5bdca8c7820a30f8e
SusLiu03 <susannl5@uci.edu>
2024-05-03 00:27:32 -0700

    finished post page

--- bitlink-app/src/app/pages/post-page/post-page.component.ts
+++ bitlink-app/src/app/pages/post-page/post-page.component.ts
@@ -4,6 +4,7 @@ import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { SearchBarComponent } from '../../components/search-bar/search-bar.component';
 import { ReplyComponent } from '../../components/reply/reply.component';
 import { CommentComponent } from '../../components/comment/comment.component';
+import { PostComponentComponent } from '../../components/post-component/post-component.component';
 @Component({
   selector: 'app-post-page',
   standalone: true,
@@ -13,6 +14,7 @@ import { CommentComponent } from '../../components/comment/comment.component';
     SearchBarComponent,
     ReplyComponent,
     CommentComponent,
+    PostComponentComponent,
   ],
   templateUrl: './post-page.component.html',
   styleUrl: './post-page.component.scss',
@@ -23,6 +25,11 @@ export class PostPageComponent {
   profile_picture2: string = 'assets/remy.jpg';
   username: string = 'C++ Chef';
   usertag: string = '@ChefCPP';
+  usertag2: string = '@RustyRustacean';
+  username2: string = 'Ferris the Crab';
+  profile_picture3: string = 'assets/ferris-on-pattern.jpg';
+  date: string = '9:19 AM Apr 13, 2024';
+  message: string = 'Rust coders be like...';
   comment: string =
     'I really like this post! I think it is very informative and helpful for people looking to learn R. However, I feel like people should look towards C++ instead. C++ is a much more practical language than R or Rust.';
 }
