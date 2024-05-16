1bc2a48dc70ae31a6990bde2c0bcb951a608e4a5
Anthony Wong <wong.anthonyjordan@gmail.com>
2024-05-15 21:53:55 -0700

    Setup for MongoDB

---
+++ server/server.ts
@@ -0,0 +1,18 @@
+import * as dotenv from "dotenv";
+// @ts-ignore
+import express from "express";
+// @ts-ignore
+import cors from "cors";
+import { connectToDatabase } from "./database";
+
+connectToDatabase()
+    .then(() => {
+        const app = express();
+        app.use(cors());
+
+        // start the Express server
+        app.listen(4200, () => {
+            console.log(`Server running at http://localhost:5200...`);
+        });
+    })
+    .catch((error) => console.error(error));
