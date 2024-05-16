d651a5c82825bbd13b6ec22f1c81b21b15508cce
SusLiu03 <susannl5@uci.edu>
2024-05-16 04:11:57 -0700

    fixed slight bugs

--- server/server.ts
+++ server/server.ts
@@ -1,18 +1,18 @@
 import * as dotenv from "dotenv";
 // @ts-ignore
-import express from "express";
+import * as express from "express";
 // @ts-ignore
-import cors from "cors";
+import * as cors from "cors";
 import { connectToDatabase } from "./database";
 
 connectToDatabase()
-    .then(() => {
-        const app = express();
-        app.use(cors());
+  .then(() => {
+    const app = express();
+    app.use(cors());
 
-        // start the Express server
-        app.listen(4200, () => {
-            console.log(`Server running at http://localhost:5200...`);
-        });
-    })
-    .catch((error) => console.error(error));
+    // start the Express server
+    app.listen(4200, () => {
+      console.log(`Server running at http://localhost:4200...`);
+    });
+  })
+  .catch((error) => console.error(error));
