6c74107c5ea26f6e7f16c5abe1aaf3337a5e07de
Hasa16 <104241399+Hasa16@users.noreply.github.com>
2024-05-17 14:40:29 -0700

    Message page WIP, moved code from database to server.ts, created a types.ts to be able to structure data and send, created a route for message page. ALL WORK IN PROGRESS, pushed via request of phil.

--- server/database.ts
+++ server/database.ts
@@ -2,7 +2,7 @@ import {MongoClient, ServerApiVersion} from 'mongodb';
 const uri = "mongodb+srv://briannw2:IuH2qY69AaAKHGSs@bitlink.wfyrdwt.mongodb.net/?retryWrites=true&w=majority&appName=Bitlink";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
+export const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
---
+++ server/types.ts
@@ -0,0 +1,9 @@
+// D:/Classes/INF124/BitLink/server/types.ts
+
+export interface User {
+    username: string;
+    usertag: string;
+    profile_pic: string;
+    user_id: string;
+  }
+  
