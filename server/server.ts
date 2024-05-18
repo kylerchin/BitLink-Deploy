5562e914f39aec7f88c1d4ac28d09fdc886730de
Phillip Peterson <58966732+r-adaR@users.noreply.github.com>
2024-05-17 22:16:33 -0700

    Posts, user profile, search post API calls.

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,11 @@
 import * as dotenv from "dotenv";
-import express, { response } from "express";
+import express, { query, response } from "express";
 import cors from "cors";
 import { User } from "./types";
+import { Post } from "./types";
+import { ProfileInfo } from "./types";
 import {MongoClient, ServerApiVersion} from 'mongodb';
+import { title } from "process";
 const uri = "mongodb+srv://briannw2:IuH2qY69AaAKHGSs@bitlink.wfyrdwt.mongodb.net/?retryWrites=true&w=majority&appName=Bitlink";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -73,6 +76,125 @@ connectToDatabase()
       } catch (error) {
         console.error("Error fetching messages:", error);
         res.status(500).send('Failed to fetch messages');
+      }
+    });
+
+    app.get('/api/posts', async (req, res) => {
+      try {
+        const database = client.db("account");
+        const posts = await database.collection("post").find().toArray();
+
+        // Map the post documents to the required format
+        const data: Post[] = posts.map(post => ({
+          title: post.title,
+          content: {
+            message: post.content.message,
+            image: post.content.image,
+            video: post.content.video
+          },
+          user: {
+            username: post.user.username,
+            usertag: post.user.usertag,
+            profile_pic: post.user.profile_pic
+          },
+          comments: post.comments,
+          timestamp: post.timestamp,
+          likes: post.likes,
+          reposts: post.reposts,
+          comment_num: post.comment_num,
+          saves: post.saves
+        }));
+
+        res.json(data);
+      } catch (error) {
+        console.error("Error fetching messages:", error);
+        res.status(500).send('Failed to fetch messages');
+      }
+    });
+
+    app.get('/api/user/:id', async (req, res) => {
+      try {
+        const database = client.db("account");
+        const user_query = {'user_tag': req.params.id};
+        const user_post_query = {'user.usertag': req.params.id};
+        const user_info = await database.collection("user").find(user_query).toArray();
+        const user_posts = await database.collection("post").find(user_post_query).toArray();
+
+        const userInfo: ProfileInfo[] = user_info.map(user => ({
+          user_id: user.user_id,
+          username: user.username,
+          usertag: user.user_tag,
+          email: user.email,
+          password: user.password,
+          profile_picture: user.profile_picture,
+          bio: user.bio,
+          following: user.following
+        }));
+
+        // Map the post documents to the required format
+        const posts: Post[] = user_posts.map(post => ({
+          title: post.title,
+          content: {
+            message: post.content.message,
+            image: post.content.image,
+            video: post.content.video
+          },
+          user: {
+            username: post.user.username,
+            usertag: post.user.usertag,
+            profile_pic: post.user.profile_pic
+          },
+          comments: post.comments,
+          timestamp: post.timestamp,
+          likes: post.likes,
+          reposts: post.reposts,
+          comment_num: post.comment_num,
+          saves: post.saves
+        }));
+
+        const data = {
+          user_info: userInfo,
+          user_posts: posts
+        }
+
+        res.json(data);
+      } catch (error) {
+        console.error("Error fetching user:", error);
+        res.status(500).send('Failed to fetch user');
+      }
+    });
+
+    app.get('/api/search/', async (req, res) => {
+      try {
+        const database = client.db("account");
+        const post_query = {'content.message': {$regex: req.query.q}}
+        const search = await database.collection("post").find(post_query).toArray();
+
+        // Map the user documents to the required format
+        const data: Post[] = search.map(post => ({
+          title: post.title,
+          content: {
+            message: post.content.message,
+            image: post.content.image,
+            video: post.content.video
+          },
+          user: {
+            username: post.user.username,
+            usertag: post.user.usertag,
+            profile_pic: post.user.profile_pic
+          },
+          comments: post.comments,
+          timestamp: post.timestamp,
+          likes: post.likes,
+          reposts: post.reposts,
+          comment_num: post.comment_num,
+          saves: post.saves
+        }));
+
+        res.json(data);
+      } catch (error) {
+        console.error("Error searching:", error);
+        res.status(500).send('Failed to search');
       }
     });
 
--- server/types.ts
+++ server/types.ts
@@ -6,4 +6,34 @@ export interface User {
     profile_pic: string;
     user_id: string;
   }
-  
+
+export interface ProfileInfo {
+    user_id: string;
+    username: string;
+    usertag: string;
+    email: string;
+    password: string;
+    profile_picture: string;
+    bio: string;
+    following: string[];
+}
+
+export interface Post {
+    title: string;
+    content: {
+        message: string;
+        image: string;
+        video: string;
+    }
+    user: {
+        username: string;
+        usertag: string;
+        profile_pic: string
+    }
+    comments: string[];
+    timestamp: string;
+    likes: number;
+    reposts: number;
+    comment_num: number;
+    saves: number;
+}
