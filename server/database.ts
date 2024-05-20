a99d84f361c1be2c709ef96a6b22981e3be02028
SusLiu03 <susannl5@uci.edu>
2024-05-20 02:15:00 -0700

    more frontend stuff

--- server/database.ts
+++ server/database.ts
@@ -1,14 +1,15 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
+import { Post } from "./types";
 const uri =
   "mongodb+srv://briannw2:IuH2qY69AaAKHGSs@bitlink.wfyrdwt.mongodb.net/?retryWrites=true&w=majority&appName=Bitlink";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 export const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
 export async function connectToDatabase() {
