ecc2ddc1e2de337d1d6e9aa83061854662a22799
SusLiu03 <susannl5@uci.edu>
2024-05-20 00:32:51 -0700

    added more frontend

--- bitlink-app/src/app/components/message-text-box/message-text-box.component.ts
+++ bitlink-app/src/app/components/message-text-box/message-text-box.component.ts
@@ -1,6 +1,6 @@
-import {Component, Input, EventEmitter, Output} from '@angular/core';
-import {CommonModule} from "@angular/common";
-import {FormsModule} from "@angular/forms";
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -9,20 +9,28 @@ import { HttpClientModule } from '@angular/common/http';
   standalone: true,
   imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './message-text-box.component.html',
-  styleUrl: './message-text-box.component.scss'
+  styleUrl: './message-text-box.component.scss',
 })
 export class MessageTextBoxComponent {
-  @Input() textField : String | undefined;
-  @Input() width : number | undefined;
-  @Input() height : number | undefined;
+  @Input() textField: String | undefined;
+  @Input() width: number | undefined;
+  @Input() height: number | undefined;
   @Input() sender_id: string | undefined;
   @Input() receiver_id: string | undefined;
-  inputValue: string = ''; 
+  inputValue: string = '';
+  senderId: string = ''; // replace with actual value
+  receiverId: string = '';
   @Output() SendMessage = new EventEmitter<string[]>();
-  submitFunction(text: string, sender_id: string, receiver_id: string) {
+  submitFunction(
+    text: string,
+    sender_id: string | undefined,
+    receiver_id: string | undefined
+  ) {
+    if (!text || !sender_id || !receiver_id) {
+      return;
+    }
     const currentTime = new Date().toISOString().replace(/\.\d{3}/, '');
-    const messageData = [text, sender_id, receiver_id, currentTime]; 
-    this.SendMessage.emit( messageData );
+    const messageData = [text, sender_id, receiver_id, currentTime];
+    this.SendMessage.emit(messageData);
   }
-
 }
