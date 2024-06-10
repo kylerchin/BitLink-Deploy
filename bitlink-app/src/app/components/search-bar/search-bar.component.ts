a33da21e7c004f4b88c331402f1336b4d417264d
Phillip Peterson <58966732+r-adaR@users.noreply.github.com>
2024-06-10 02:34:00 -0700

    Search implementation.

--- bitlink-app/src/app/components/search-bar/search-bar.component.ts
+++ bitlink-app/src/app/components/search-bar/search-bar.component.ts
@@ -1,13 +1,25 @@
 import { Component, Input } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { query } from 'express';
+
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, FormsModule, ReactiveFormsModule],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.scss',
 })
 export class SearchBarComponent {
+  constructor(private router: Router) {}
   @Input() textField: String | undefined;
+  searchQuery: string = "";
+
+  onSubmit() {
+    if (this.searchQuery) {
+      this.router.navigate(['/search'], {queryParams: {q: this.searchQuery}});
+    }
+  }
 }
